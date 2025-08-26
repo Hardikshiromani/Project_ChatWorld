1f79c678d15d638e87e9cadac0df822b95899c0b
Hardik Shiromani <hardikshiromani786@gmail.com>
2025-08-26 17:33:24 +0530

    updated_ gitignore

--- Backend/firebaseConfig.js
+++ Backend/firebaseConfig.js
@@ -11,13 +11,34 @@
 // });
 
 // module.exports =admin;
+// const admin = require("firebase-admin");
+// const path = require("path");
+
+// // Use Render secret file if available, otherwise local file
+// const serviceAccountPath = process.env.GOOGLE_APPLICATION_CREDENTIALS 
+//   ? process.env.GOOGLE_APPLICATION_CREDENTIALS 
+//   : path.join(__dirname, "serviceKey.json");
+
+// const serviceAccount = require(serviceAccountPath);
+
+// admin.initializeApp({
+//   credential: admin.credential.cert(serviceAccount),
+// });
+
+// module.exports = admin;
 const admin = require("firebase-admin");
 const path = require("path");
+const fs = require("fs");
 
-// Use Render secret file if available, otherwise local file
-const serviceAccountPath = process.env.GOOGLE_APPLICATION_CREDENTIALS 
-  ? process.env.GOOGLE_APPLICATION_CREDENTIALS 
-  : path.join(__dirname, "serviceKey.json");
+let serviceAccountPath;
+
+// On Render, secret files are stored in /etc/secrets/
+if (fs.existsSync("/etc/secrets/serviceKey.json")) {
+  serviceAccountPath = "/etc/secrets/serviceKey.json";
+} else {
+  // Local fallback (when running on your laptop)
+  serviceAccountPath = path.join(__dirname, "serviceKey.json");
+}
 
 const serviceAccount = require(serviceAccountPath);
 
