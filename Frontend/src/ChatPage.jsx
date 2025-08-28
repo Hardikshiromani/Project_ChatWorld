713ab2386a24ee794d696ddaee3701f81bfb5661
Hardik Shiromani <hardikshiromani786@gmail.com>
2025-08-28 16:28:04 +0530

    imports updated

--- Frontend/src/ChatPage.jsx
+++ Frontend/src/ChatPage.jsx
@@ -11,10 +11,12 @@
 // import axios from "axios"; // Axios for API calls
 // import socket from "../socket"; // Import socket instance for real-time communication
 import React, { useState, useEffect } from "react";
-import Sidebar from './Chat/SideBar';       // ✅ matches folder + file
-import ChatList from './Chat/ChatList';     // ✅ lowercase "c" matches file
-import ChatRoom from "./Chat/ChatRoom";     // ✅ exact match
-import UserProfile from "./Chat/Userprofile.jsx"; // ✅ "Userprofile" matches file
+import Sidebar from "./chat/SideBar";       // ✅ matches folder + file
+import ChatList from "../chat/ChatList";     // ✅ lowercase "c" matches file
+import ChatRoom from "../chat/ChatRoom";     // ✅ exact match
+// import  UserProfile from "./chat/UserProfile.jsx";
+ import UserProfile from "../chat/UserProfile"; // Displays user profile
+
 import Status from "./Status";
 import Contacts from "./Contacts";
 import Settings from "./Settings";
