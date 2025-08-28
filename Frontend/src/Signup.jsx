8b9343e1aace167dd8cec29025ba9eefe89bd1ce
Hardik Shiromani <hardikshiromani786@gmail.com>
2025-08-28 10:11:27 +0530

    frontend fixing

--- Frontend/src/Signup.jsx
+++ Frontend/src/Signup.jsx
@@ -1,7 +1,7 @@
 
 import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
-import { auth, RecaptchaVerifier, signInWithPhoneNumber } from "../firebase";
+import { auth, RecaptchaVerifier, signInWithPhoneNumber } from "./firebase";
 import axios from "axios";
 
 
