2e04087133474625492f2847c9350cb5a10ae2c7
Hardik Shiromani <hardikshiromani786@gmail.com>
2025-08-28 10:06:10 +0530

    frontend updated

--- Frontend/src/Profilepage.jsx
+++ Frontend/src/Profilepage.jsx
@@ -2,7 +2,7 @@
 import React, { useState } from "react";
 import axios from "axios";
 import { useNavigate } from "react-router-dom";
-import { auth } from "../firebase";
+import { auth } from "./firebase";
 
 const Profilepage = () => {
   const [userName, setUsername] = useState("");
