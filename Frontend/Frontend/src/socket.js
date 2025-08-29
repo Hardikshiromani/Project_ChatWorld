2efa96aec0b126b678a3da484cbdfaeea5cc8fbe
Hardikshiromani <hardikshiromani786@gmail.com>
2025-08-29 18:52:37 +0530

    fixed duplicates

--- Frontend/Frontend/src/socket.js
+++
@@ -1,8 +0,0 @@
-import {io} from 'socket.io-client';
-const BackURL=import.meta.env.VITE_API_URL;
-
- const socket = io(`${BackURL}`, {
-    transports: ["websocket"], // Optional, for better real-time
-});
-
-export default socket;
