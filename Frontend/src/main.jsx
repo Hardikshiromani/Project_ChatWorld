c7fefd71290cf96e3f019d98961240260a812a1c
Hardikshiromani <hardikshiromani786@gmail.com>
2025-08-31 18:20:40 +0530

    fixing vercel and vite config

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -1,12 +1,15 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom'
+// import { BrowserRouter } from 'react-router-dom'
+import { HashRouter } from 'react-router-dom'
 import App from './App.jsx'
 // import { getMessaging, getToken, onMessage } from './pages/firebase'; 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-     <BrowserRouter>
+     {/* <BrowserRouter> */}
+     <HashRouter>
       <App />
-    </BrowserRouter>
+    {/* </BrowserRouter> */}
+</HashRouter>
   </StrictMode>
 )
--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -4,7 +4,8 @@ import react from '@vitejs/plugin-react-swc'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-   build: {
+  base:"./",
+  build: {
     outDir: 'dist', // Vercel expects static assets here
   },
 })
